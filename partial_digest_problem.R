e2625d95de80245e199800d0810da6bdb77b8356
David Slavicek <211212@vut.cz>
2021-10-21 14:45:17 +0200

    continue working on partial digest problem

--- partial_digest_problem.R
+++ partial_digest_problem.R
@@ -5,22 +5,32 @@ partial_digest_problem <- function(L){
   return(place(L, X, width))
 }
 place <- function(L, X, width){
+  print("place")
+  print("L:")
+  print(L)
+  print("X:")
+  print(X)
   if(length(L) == 0){
+    print("result:")
     print(X)
     return(X)
   }
   y <- max(L)
   
-  y_X <- y - X
-  if(prod(L %in% y_X)){
+  y_X <- abs(y - X)
+  print("y_x:")
+  print(y_X)
+  if(prod(y_X %in% L)){
+    print("prvni if")
     X <- c(X, y)
     L <- which(!(L %in% y_X))
     place(L, X, width)
     X <- X[1:length(X)-1] # spoliham na to, ze y zustane poslednim prvkem X
     L <- c(L, y_X)
   }
-  w_y_X <- width - y - X
-  if(prod(L %in% w_y_X)){
+  w_y_X <- abs(width - y - X)
+  if(prod(w_y_X %in% L)){
+    print("druhej if")
     X <- c(X, width - y)
     L <- which(!(L %in% w_y_X))
     place(L, X, width)
