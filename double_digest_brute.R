0d9e289b237265885b1fe0003da7c023a06bb8ef
David Slavicek <211212@vut.cz>
2021-10-21 13:06:30 +0200

    Started implementing Double Digest Problem. Not working yet.

---
+++ double_digest_brute.R
@@ -0,0 +1,56 @@
+double_digest_brute = function(fragmentsA, fragmentsB, fragmentsAB){
+  # 1. sort fragments
+  #fragmentsA <- sort(fragmentsA)
+  #fragmentsB <- sort(fragmentsB)
+  fragmentsAB <- sort(fragmentsAB)
+  
+  # 2. order fragments
+  # iteration through all permutations needed
+  
+  # 3. create fragments map
+  restriction_mapA <- c(0, cumsum(fragmentsA))
+  restriction_mapB <- c(0, cumsum(fragmentsB))
+  print(restriction_mapA)
+  print(restriction_mapB)
+  # 4. merge positions
+  restriction_map_merged <- c()
+  pointerA <- 1
+  pointerB <- 1
+  while(pointerA <= length(restriction_mapA) &&
+        pointerB <= length(restriction_mapB)){
+    print(restriction_mapA[pointerA])
+    print(restriction_mapB[pointerB])
+    print(restriction_mapA[pointerA] >= restriction_mapB[pointerB])
+    if(restriction_mapA[pointerA] >= restriction_mapB[pointerB]){
+      restriction_map_merged <- c(restriction_map_merged, restriction_mapA[pointerA])
+      pointerA <- pointerA + 1
+    }else{
+      restriction_map_merged <- c(restriction_map_merged, restriction_mapB[pointerB])
+      pointerB <- pointerB + 1
+    }
+  }
+  restriction_map_merged <- unique(restriction_map_merged)
+  # 5. difference
+  fragments_merged <- diff(restriction_map_merged)
+  # 6. sort, compare with fragmentsAB
+  fragments_merged <- sort(fragments_merged)
+  
+  if(prod(fragments_merged == fragmentsAB)){
+    print("Reseni nalezeno")
+    return(restriction_map_merged)
+    
+    # 7. Reverse solution
+  }
+  
+  
+}
+
+perm <- function(v) {
+  n <- length(v)
+  if (n == 1) v
+  else {
+    X <- NULL
+    for (i in 1:n) X <- rbind(X, cbind(v[i], perm(v[-i])))
+    X
+  }
+}
