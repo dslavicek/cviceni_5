71ba28fb5132d9f61ad8a9d6148eb5c293730066
David Slavicek <211212@vut.cz>
2021-10-21 13:33:17 +0200

    Remove unused code

--- double_digest_brute.R
+++ double_digest_brute.R
@@ -16,21 +16,7 @@ double_digest_brute = function(fragmentsA, fragmentsB, fragmentsAB){
   restriction_map_merged <- c(restriction_mapA, restriction_mapB)
   restriction_map_merged <- sort(restriction_map_merged)
   restriction_map_merged <- unique(restriction_map_merged)
-  
-  # while(pointerA <= length(restriction_mapA) &&
-  #       pointerB <= length(restriction_mapB)){
-  #   print(restriction_mapA[pointerA])
-  #   print(restriction_mapB[pointerB])
-  #   print(restriction_mapA[pointerA] >= restriction_mapB[pointerB])
-  #   if(restriction_mapA[pointerA] >= restriction_mapB[pointerB]){
-  #     restriction_map_merged <- c(restriction_map_merged, restriction_mapA[pointerA])
-  #     pointerA <- pointerA + 1
-  #   }else{
-  #     restriction_map_merged <- c(restriction_map_merged, restriction_mapB[pointerB])
-  #     pointerB <- pointerB + 1
-  #   }
-  # }
-  
+
   # 5. difference
   fragments_merged <- diff(restriction_map_merged)
   # 6. sort, compare with fragmentsAB
@@ -44,14 +30,4 @@ double_digest_brute = function(fragmentsA, fragmentsB, fragmentsAB){
   }
   
   
-}
-
-perm <- function(v) {
-  n <- length(v)
-  if (n == 1) v
-  else {
-    X <- NULL
-    for (i in 1:n) X <- rbind(X, cbind(v[i], perm(v[-i])))
-    X
-  }
 }
